cdf4acb8cb283340f419b476faa92d769931ef37
Andrew Falgout <andrew.falgout@gmail.com>
2023-07-21 14:13:58 -0500

    added just file merging feature

--- repo2text.py
+++ repo2text.py
@@ -2,7 +2,6 @@
 import os
 import subprocess
 import argparse
-import requests
 from bs4 import BeautifulSoup
 
 def clone_repo(repo_url):
@@ -59,7 +58,7 @@ def scrape_doc(doc_url):
   soup = BeautifulSoup(response.content, 'html.parser')
   return soup.get_text(separator="\n")
 
-def write_text_file(repo_name, file_data, doc_text):
+def write_text_file(repo_name, file_data, doc_text=None):
     filename = f"{repo_name}.txt"
     with open(filename, "w") as f:
         if doc_text:
@@ -69,9 +68,28 @@ def write_text_file(repo_name, file_data, doc_text):
             f.write(data)
     print(f"Text file saved: {filename}")
 
+def get_local_file_contents(file_path):
+    if os.path.isdir(file_path):
+        contents = []
+        for root, dirs, files in os.walk(file_path):
+            for file in files:
+                full_path = os.path.join(root, file)
+                if os.path.isfile(full_path):
+                    with open(full_path, 'r') as f:
+                        content = f.read()
+                    contents.append(f"\\n'''--- {full_path} ---\\n{content}\\n'''\\n")
+        return contents
+    elif os.path.isfile(file_path):
+        with open(file_path, 'r') as f:
+            content = f.read()
+        return [f"\\n'''--- {file_path} ---\\n{content}\\n'''\\n"]
+    else:
+        print(f"Path {file_path} does not exist.")
+        return None
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--repo", required=True, help="GitHub repo URL")
+    parser.add_argument("-r", "--repo", required=False, help="GitHub repo URL")
     parser.add_argument("-d", "--doc", required=False, help="Documentation URL")
     parser.add_argument(
         "-t",
@@ -80,12 +98,31 @@ if __name__ == "__main__":
         nargs="+",
         help="File extensions to include (without dot), e.g., py, md, html, json",
     )
+    parser.add_argument(
+        "files",
+        metavar="F",
+        type=str,
+        nargs="*",
+        help="Files to include in the output",
+    )
     args = parser.parse_args()
-    repo_name = clone_repo(args.repo)
-    file_data = walk_dir(repo_name, args.types)
+    
+    file_data = []
+    
+    if args.repo:
+        repo_name = clone_repo(args.repo)
+        file_data.extend(walk_dir(repo_name, args.types))
+    else:
+        repo_name = os.path.basename(os.getcwd())
+    
+    for file_path in args.files:
+        content = get_local_file_contents(file_path)
+        if content is not None:
+            file_data.extend(content)
 
     doc_text = ""
     if args.doc:
         doc_text = scrape_doc(args.doc)
 
     write_text_file(repo_name, file_data, doc_text)
+
