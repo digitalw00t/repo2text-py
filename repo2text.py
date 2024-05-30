89350008b64d6a35695a36443caa8b2f6c513fe3
draeician <draeician@gmail.com>
2024-05-29 22:24:22 -0500

    Expand IGNORED_EXTENSIONS to include various compiled, media, archive, and application-specific file types

--- repo2text.py
+++ repo2text.py
@@ -9,7 +9,42 @@ import fnmatch
 
 __VERSION__ = "v1.1.0"
 
-IGNORED_EXTENSIONS = [".log", ".exe"]
+IGNORED_EXTENSIONS = [
+        # Compiled executables and libraries
+        '.exe', '.dll', '.so', '.a', '.lib', '.dylib', '.o', '.obj',
+        # Compressed archives
+        '.zip', '.tar', '.tar.gz', '.tgz', '.rar', '.7z', '.bz2', '.gz', '.xz', '.z', '.lz', '.lzma', '.lzo', '.rz', '.sz', '.dz',
+        # Application-specific files
+        '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.odt', '.ods', '.odp',
+        # Media files (less common)
+        '.png', '.jpg', '.jpeg', '.gif', '.mp3', '.mp4', '.wav', '.flac', '.ogg', '.avi', '.mkv', '.mov', '.webm', '.wmv', '.m4a', '.aac',
+        # Virtual machine and container images
+        '.iso', '.vmdk', '.qcow2', '.vdi', '.vhd', '.vhdx', '.ova', '.ovf',
+        # Database files
+        '.db', '.sqlite', '.mdb', '.accdb', '.frm', '.ibd', '.dbf',
+        # Java-related files
+        '.jar', '.class', '.war', '.ear', '.jpi',
+        # Python bytecode and packages
+        '.pyc', '.pyo', '.pyd', '.egg', '.whl',
+        # Other potentially important extensions
+        '.deb', '.rpm', '.apk', '.msi', '.dmg', '.pkg', '.bin', '.dat', '.data',
+        '.dump', '.img', '.toast', '.vcd', '.crx', '.xpi', '.lockb', 'package-lock.json', '.svg' ,
+        '.eot', '.otf', '.ttf', '.woff', '.woff2',
+        '.ico', '.icns', '.cur',
+        '.cab', '.dmp', '.msp', '.msm',
+        '.keystore', '.jks', '.truststore', '.cer', '.crt', '.der', '.p7b', '.p7c', '.p12', '.pfx', '.pem', '.csr',
+        '.key', '.pub', '.sig', '.pgp', '.gpg',
+        '.nupkg', '.snupkg', '.appx', '.msix', '.msp', '.msu',
+        '.deb', '.rpm', '.snap', '.flatpak', '.appimage',
+        '.ko', '.sys', '.elf',
+        '.swf', '.fla', '.swc',
+        '.rlib', '.pdb', '.idb', '.pdb', '.dbg',
+        '.sdf', '.bak', '.tmp', '.temp', '.log', '.tlog', '.ilk',
+        '.bpl', '.dcu', '.dcp', '.dcpil', '.drc',
+        '.aps', '.res', '.rsrc', '.rc', '.resx',
+        '.prefs', '.properties', '.ini', '.cfg', '.config', '.conf',
+        '.DS_Store', '.localized', '.svn', '.git', '.gitignore', '.gitkeep',
+    ]
 
 def get_gitignore_patterns(repo_path):
     gitignore_path = os.path.join(repo_path, '.gitignore')
